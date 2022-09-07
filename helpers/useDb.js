06fbd204d6ee0b4710754695ac7089d661ac3d00
roshan.dev <roshan.dev@al-ghurair.com>
2022-09-07 10:49:40 +0400

    implemented deploy requirements for kubernetes

--- helpers/useDb.js
+++ helpers/useDb.js
@@ -1,13 +1,28 @@
 const mongoose = require('mongoose');
-const {config: {MONGO_URL, mongodbOptions}} = require('../config');
+const {
+  config: { MONGO_URL, MAX_MONGO_RETRY, mongodbOptions },
+} = require('../config');
 const db = mongoose.connection;
-if(!db.readyState){
-  mongoose.connect(MONGO_URL, mongodbOptions);
+
+function connect() {
+  console.log('mongo connecting...');
+  mongoose.connect(MONGO_URL, mongodbOptions).catch(err => console.error('Mongo error.....', err.toString()));
 }
+
+if (!db.readyState) connect();
+
+let retry = MAX_MONGO_RETRY;
 
 module.exports = new Promise((resolve, reject) => {
   //for successful connection
   db.once('open', () => resolve(db));
   //for error in connection
-  db.on('error', reject);
+  db.on('error', (err) => {
+    if (!['ECONNREFUSED'].includes(err.code)) retry = 0;
+    else {
+      retry = --retry;
+      setTimeout(connect, 3000);
+    }
+    if (!retry) return reject(err.toString());
+  });
 });
