c5ad9cf6eaedd934b0d7c6b5c3fab774344d5d45
roshan.dev <roshan.dev@al-ghurair.com>
2022-09-08 17:54:45 +0400

    removed using dotenv

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const logger = require('./middlewares/logger');
 const app = express();
-require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
