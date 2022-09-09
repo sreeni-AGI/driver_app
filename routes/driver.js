2d888dd5c8eb0bc163622da760c92e94848b7e79
mohammad-tanzeel <tanzeel.tech@gmail.com>
2022-09-09 18:03:46 +0400

    remove comments in crud

--- routes/driver.js
+++ routes/driver.js
@@ -1,21 +1,4 @@
-// CRUD
-
-
-// take projection mind
-
-
-// ger /docs  => roshan
-//get / => collins
-// get /:id = colins
-// post /  => tanzeel
-// put /:id => tanzeel
-//patch /:id   => roshan
-
-//delete /:id => tanzeel
-
-
 const driverController = require('../controller/driver.controller');
-
 const router = require('express').Router();
 
 router.post('/', driverController.create);
