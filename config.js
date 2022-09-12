55c70c862b3b36f07c34bca9e6ce3c87ba3887b9
Roshan Dev <46485420+rdev06@users.noreply.github.com>
2022-09-12 15:31:10 +0400

    Fixed deployement files (#9)
    * WIP
    
    * added find and findOne for driver
    
    * configured devops file
    
    * delete env files
    
    * ignore env files in git
    
    * config devops
    
    * fixed deploy settings
    
    * fixed sms uri
    
    * master otp
    
    Co-authored-by: roshan.dev <roshan.dev@al-ghurair.com>

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 const { configModel } = require("./model");
 
 const {
+  NODE_ENV,
   MAX_REDIS_RETRY,
   MAX_MONGO_RETRY,
   REDIS_HOST,
@@ -14,13 +15,13 @@ const {
   MONGO_USER,
   MONGO_PASS,
   DRIVER_API_URL,
-  DRIVER_API_KEY,
   DRIVER_OUTSTANDING_API_KEY,
   DRIVER_COLLECTION_API_KEY
 } = process.env;
 
 module.exports = {
   config: {
+    isDevelopment: NODE_ENV === 'dev',
     MAX_REDIS_RETRY,
     MAX_MONGO_RETRY,
     REDIS_HOST,
@@ -32,7 +33,6 @@ module.exports = {
     JWT_SECRET,
     MONGO_URL,
     DRIVER_API_URL,
-    DRIVER_API_KEY,
     DRIVER_OUTSTANDING_API_KEY,
     DRIVER_COLLECTION_API_KEY,
     mongodbOptions: {
