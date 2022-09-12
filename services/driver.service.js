4f18dca092396a84dc906dcbe7d2c05607b2a22f
mohammad-tanzeel <tanzeel.tech@gmail.com>
2022-09-12 18:22:07 +0400

    Merge branch 'dev' of https://github.com/sreeni-AGI/driver_app into tanzeel

--- services/driver.service.js
+++ services/driver.service.js
@@ -1,8 +1,9 @@
 const { driverModel } = require("../model");
 module.exports = {
-  findOne: (filter, projection = {}) => driverModel.findOne(filter, projection).lean(),
+  find: (filter, projection = {}) => driverModel.find(filter, projection).lean(),
   findOne: (filter, projection = {}) => driverModel.findOne(filter, projection).lean(),
   create: driverData => driverModel.create(driverData),
   updateOne: (staffId, driverData) => driverModel.replaceOne({ staffId }, driverData),
   deleteOne: staffId => driverModel.updateOne({ staffId }, { $set: { isDeleted: true } })
 };
+
