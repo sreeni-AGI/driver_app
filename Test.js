db3351dfa58ed18a0e81bd766f0ed22338b9d254
mohammad-tanzeel <tanzeel.tech@gmail.com>
2022-09-15 11:35:44 +0400

    remove Test File

--- Test.js
+++
@@ -1,5 +0,0 @@
-var currentDate = new Date();
-    const endDate = new Date();
-    endDate.setUTCHours(23,59,59)
-    const timediff = parseInt(((endDate.getTime())-(currentDate.getTime() + (3600000*4)))/1000);
-    console.log(timediff)
